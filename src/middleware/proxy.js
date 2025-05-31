3bca9a05c7d59e797b1cee657d9c44f48ff74ac2
ziaulkamal <ziaul.kamal1109@gmail.com>
2025-05-31 13:42:39 +0700

    update

--- src/middleware/proxy.js
+++ src/middleware/proxy.js
@@ -1,6 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { ROOT_TARGET } = require('../config/env');
 
+if (!ROOT_TARGET) {
+    throw new Error('ROOT_TARGET is not defined. Check your .env file.');
+}
+
 const proxyMiddleware = createProxyMiddleware({
     target: ROOT_TARGET,
     changeOrigin: true,
